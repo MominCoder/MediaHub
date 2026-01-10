b6e3e0935641a79ca32cd3410b4f7a4f237ec0e9
Muzammil Momin <mominmuzammil157@gmail.com>
2026-01-11 04:29:46 +0530

    create watchpage video container

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -1,7 +1,22 @@
-const WatchPage = () => {
-  return (
-    <div>WatchPage</div>
-  )
-}
+import { useSearchParams } from "react-router-dom";
 
-export default WatchPage
+const WatchPage = () => {
+  const [searchParams] = useSearchParams();
+  const videoId = searchParams.get("v");
+
+  return (
+    <div className="w-2/3">
+      <iframe
+      className="rounded-xl h-80"
+        width="100%"
+        height="100%"
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&showinfo=0`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default WatchPage;
