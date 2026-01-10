fd55432453b3eba62319444123d23d11c57ae8a5
Muzammil Momin <mominmuzammil157@gmail.com>
2026-01-10 22:27:40 +0530

    list all tabs together

---
+++ src/components/ButtonList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from './Button'
+
+const ButtonList = () => {
+  return (
+    <div className='flex gap-2'>
+      <Button name={'All'} />
+      <Button name={'Shorts'} />
+    </div>
+  )
+}
+
+export default ButtonList
