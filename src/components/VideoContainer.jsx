c11ee4240639bc73aaf1db2e52835b82b4f5947e
Muzammil Momin <mominmuzammil157@gmail.com>
2026-01-10 22:31:42 +0530

    fetch videos from youtube api and pass to VideoCard component

---
+++ src/components/VideoContainer.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import VideoCard from "./VideoCard";
+import { YOUTUBE_VIDEOS_API } from "../utils/constants";
+import { Link } from "react-router-dom";
+
+const VideoContainer = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const getVideos = async () => {
+      const response = await fetch(YOUTUBE_VIDEOS_API);
+      const data = await response.json();
+      setVideos(data.items);
+    };
+
+    getVideos();
+  }, []);
+
+  return (
+    <div className="grid grid-cols-4 gap-5">
+      {videos &&
+        videos.map((video) => (
+          <Link to={"/watch?v=" + video.id} key={video.id}>
+            <VideoCard info={video} />
+          </Link>
+        ))}
+    </div>
+  );
+};
+
+export default VideoContainer;
