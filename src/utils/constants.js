49d2518fee33bdca6f2d2d521cb818be5bfd863b
Muzammil Momin <mominmuzammil157@gmail.com>
2026-01-11 04:29:03 +0530

    add youtube suggestions API path

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
-export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${import.meta.env.VITE_YOUTUBE_API_KEY}`
+export const YT_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${import.meta.env.VITE_YOUTUBE_API_KEY}`
+
+export const YT_SUGGETIONS_API = `http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=`
