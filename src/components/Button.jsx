be8e994d5f334197f445d48c8ea2f76029610660
Muzammil Momin <mominmuzammil157@gmail.com>
2026-01-10 22:27:14 +0530

    create button component to display filter tabs

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({ name }) => {
+  return (
+    <div className="px-2.5 py-1.5 rounded bg-gray-200 text-center font-medium cursor-pointer">
+      {name}
+    </div>
+  );
+};
+
+export default Button;
