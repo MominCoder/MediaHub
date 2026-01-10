2ba11834ac93f23661d0724651ea8393fca2aa58
Muzammil Momin <mominmuzammil157@gmail.com>
2026-01-11 04:31:17 +0530

    feature: search suggestions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,31 @@ import { useDispatch } from "react-redux";
 import { toggleSidebar } from "../utils/appSlice";
 import { useMobileSearch } from "../hooks/useMobileSearch";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { YT_SUGGETIONS_API } from "../utils/constants";
 
 const Header = () => {
   const dispatch = useDispatch();
+  const [query, setQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showsSuggestions, setShowSuggestions] = useState(false);
 
   const { isMobileSearchOpen, openMobileSearch, closeMobileSearch } =
     useMobileSearch();
+
+  const getSearchSuggestions = async () => {
+    const response = await fetch(YT_SUGGETIONS_API + query);
+    const data = await response.json();
+    setSuggestions(data[1]);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => getSearchSuggestions(), 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [query]);
 
   return (
     <header className="px-2.5 sm:px-10 xl:px-5 py-2 flex items-center justify-between gap-5">
@@ -48,38 +67,74 @@ const Header = () => {
               />
             </button>
 
-            <div className="flex w-full">
-              <input
-                type="search"
-                placeholder="Search"
-                className="pl-5 py-1 sm:py-2 w-full border border-gray-400 rounded-l-full outline-blue-400"
-              />
-              <button className="px-4 border border-l-0 border-gray-400 rounded-r-full bg-gray-100 hover:bg-gray-200">
-                <img
-                  src="/public/assets/images/search.svg"
-                  alt="Search"
-                  className="h-5"
+            <div className="relative flex w-full flex-col">
+              <div className="w-full flex">
+                <input
+                  onChange={(e) => setQuery(e.target.value)}
+                  onFocus={() => setShowSuggestions(true)}
+                  onBlur={() => setShowSuggestions(false)}
+                  type="search"
+                  placeholder="Search"
+                  className="pl-5 py-1 sm:py-2 w-full text-sm border border-gray-400 rounded-l-full outline-blue-400"
                 />
-              </button>
+                <button className="px-4 border border-l-0 border-gray-400 rounded-r-full bg-gray-100 hover:bg-gray-200">
+                  <img
+                    src="/public/assets/images/search.svg"
+                    alt="Search"
+                    className="h-5"
+                  />
+                </button>
+              </div>
+              {showsSuggestions && suggestions.length > 0 && (
+                <ul className="mt-2.5 p-2.5 shadow border border-cyan-100 rounded-xl border-t-0 bg-white absolute top-8 z-20 w-full max-h-[80vh] overflow-y-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+                  {suggestions &&
+                    suggestions.map((s) => (
+                      <li
+                        className="hover:bg-sky-100 px-3.5 py-2 rounded-md select-none text-sm"
+                        key={s}
+                      >
+                        {s}
+                      </li>
+                    ))}
+                </ul>
+              )}
             </div>
           </div>
         )}
 
         {/* DESKTOP SEARCH (always visible when mobile search is closed) */}
         {!isMobileSearchOpen && (
-          <div className="hidden min-[431px]:flex w-full max-w-xl">
-            <input
-              type="search"
-              placeholder="Search"
-              className="flex-1 px-4 py-2 w-full border border-gray-400 rounded-l-full outline-blue-400"
-            />
-            <button className="px-4 border border-l-0 border-gray-400 rounded-r-full bg-gray-100 hover:bg-gray-200">
-              <img
-                src="/public/assets/images/search.svg"
-                alt="Search"
-                className="max-h-4 sm:max-h-6"
+          <div className="relative w-full max-w-xl">
+            <div className="hidden min-[431px]:flex w-full max-w-xl">
+              <input
+                onChange={(e) => setQuery(e.target.value)}
+                type="search"
+                placeholder="Search"
+                className="flex-1 px-4 py-2 w-full border border-gray-400 rounded-l-full outline-blue-400"
+                onFocus={() => setShowSuggestions(true)}
+                onBlur={() => setShowSuggestions(false)}
               />
-            </button>
+              <button className="px-4 border border-l-0 border-gray-400 rounded-r-full bg-gray-100 hover:bg-gray-200">
+                <img
+                  src="/public/assets/images/search.svg"
+                  alt="Search"
+                  className="max-h-4 sm:max-h-6"
+                />
+              </button>
+            </div>
+            {showsSuggestions && suggestions.length > 0 && (
+              <ul className="mt-2.5 p-2.5 shadow border border-cyan-100 rounded-xl border-t-0 bg-white absolute z-20 w-full max-h-[80vh] overflow-y-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+                {suggestions &&
+                  suggestions.map((s) => (
+                    <li
+                      className="hover:bg-sky-100 px-3.5 py-2 rounded-md select-none"
+                      key={s}
+                    >
+                      {s}
+                    </li>
+                  ))}
+              </ul>
+            )}
           </div>
         )}
       </div>
